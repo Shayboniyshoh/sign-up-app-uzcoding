af0115ad88f86c77dd4501a76a2ebd12dc7490a7
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2022-06-08 10:31:13 +0500

    added sign out button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String id = "home-screen";
@@ -6,13 +9,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'You successfully signed in!',
-          style: TextStyle(
-            fontSize: 24,
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                'You successfully signed in!',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then(
+                      (value) => {
+                        Navigator.pushReplacementNamed(context, LoginScreen.id),
+                      },
+                    );
+              },
+              child: const Text(
+                'Sign Out',
+                style: TextStyle(color: Colors.black, fontSize: 18),
+              ),
+            ),
+          ],
         ),
       ),
     );
